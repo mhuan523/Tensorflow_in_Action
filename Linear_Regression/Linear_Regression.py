27a0da6562432b044a04d410938e818c381f8ad1
huan <914892930@qq.com>
2019-10-29 16:54:10 +0800

    tensorflow LinearRegression

---
+++ Linear_Regression/Linear_Regression.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import tensorflow as tf
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+X = tf.constant(housing_data_plus_bias, tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), tf.float32, name="y")
+
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    print(theta.eval())
