bdcd23241bd89dc9d105c7444035680ca2a05045
huan <914892930@qq.com>
2019-10-30 11:25:35 +0800

    Tensorflow LinearRegression GradientDescent Optimizer

---
+++ GradientDescent/GradientDescentOptimizer.py
@@ -0,0 +1,32 @@
+# conding: utf-8
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import fetch_california_housing
+import numpy as np
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+
+scaler = StandardScaler()
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+housing_data_plus_bias_scaled = scaler.fit_transform(housing_data_plus_bias)
+
+n_epochs = 10000
+learning_rate = 0.01
+X = tf.constant(housing_data_plus_bias_scaled, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(mse)
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(train_op)
+    best_theta = theta.eval()
